d530d92d6a38595d31671191f4b64029de074e7e
KickoKicko <kickokicko110@gmail.com>
2025-08-07 19:30:24 +0200

    add bubble sort

---
+++ algorithms/bubble_sort.py
@@ -0,0 +1,15 @@
+def bubble_sort(list):
+    while True:
+        flag = False
+        for i in range(len(list)-1):
+            if(list[i]>list[i+1]):
+                switch_values(list, i,i+1)
+                flag = True
+        if(not flag):
+           break
+
+
+def switch_values(list, i1, i2):
+    t = list[i1]
+    list[i1] = list[i2]
+    list[i2] = t
---
+++ main.py
@@ -0,0 +1,6 @@
+from algorithms import bubble_sort
+
+list = [5,1,67,9,3,2,0,-66,150]
+print("Before: "+ str(list))
+bubble_sort.bubble_sort(list)
+print("After: "+ str(list))
